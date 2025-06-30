d8f20ccfee82258994b74916e5ff50976d718694
javalulu <yuenzejack@gmail.com>
2025-06-30 16:22:41 +0700

    将上传的文件保存本地Multipart.transferTo()，再使用UUID构建新的不重复的文件名。更改application.yml文件来设置文件上传大小限制。

--- tlias-web-management/src/main/java/com/americano/controller/UploadController.java
+++ tlias-web-management/src/main/java/com/americano/controller/UploadController.java
@@ -6,13 +6,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 @Slf4j
 @RestController
 public class UploadController {
 
     @PostMapping("/upload")
-    public Result upload(String name, Integer age, MultipartFile file) {
+    public Result upload(String name, Integer age, MultipartFile file) throws IOException {
         log.info("接收参数： {}, {}, {}",  name, age, file);
+        //获取原始文件名
+        String originalFilename = file.getOriginalFilename();
+
+        //新的文件名
+        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String newFileName = UUID.randomUUID().toString() + extension;
+        //保存文件
+        file.transferTo(new File("C:/Users/Administrator/Pictures/" + newFileName));
+
         return Result.success();
     }
 }
