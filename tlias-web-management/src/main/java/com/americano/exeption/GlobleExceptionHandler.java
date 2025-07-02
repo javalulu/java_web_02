24226a634692c3f90e02450667be9aee38f58886
javalulu <yuenzejack@gmail.com>
2025-07-02 21:51:53 +0700

    添加对DuplicateKeyException的专用异常处理并从错误信息中获取重复的key值并返回前端

--- tlias-web-management/src/main/java/com/americano/exeption/GlobleExceptionHandler.java
+++ tlias-web-management/src/main/java/com/americano/exeption/GlobleExceptionHandler.java
@@ -2,6 +2,7 @@ package com.americano.exeption;
 
 import com.americano.pojo.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +17,17 @@ public class GlobleExceptionHandler {
     public Result handleException(Exception e) {
         log.error("程序出错", e);
         return Result.error("出错了！");
+    }
+
+    @ExceptionHandler
+    public Result handleDuplicateKeyException(DuplicateKeyException e) {
+        log.error("程序出错", e);
+        // 截取错误信息
+        String message = e.getMessage();
+        int i = message.indexOf("Duplicate entry");
+        String errMsg = message.substring(i);
+        // 找到重复的key值
+        String[] arr = errMsg.split(" ");
+        return Result.error(arr[2] + "已存在");
     }
 }
